b4d1a3c2c8e3b30499da5a48209da2930b8fab3b
Schengatto <schengatto@gmail.com>
2026-03-24 09:32:30 +0100

    feat: improve button visibility and redesign message screen as greeting card

--- app/components/viewer/CardMessage.vue
+++ app/components/viewer/CardMessage.vue
@@ -17,10 +17,14 @@ const emit = defineEmits<{
 
 <template>
   <div class="card-message">
-    <div class="card-message-name">{{ recipientName }}</div>
-    <div class="card-message-greeting">{{ $t('message.greeting') }}</div>
-    <div class="card-message-text">{{ message }}</div>
-    <div class="card-message-sender">{{ $t('message.sender', { name: senderName }) }}</div>
+    <div class="card-message-card">
+      <div class="card-message-deco">&#10045; &#10045; &#10045;</div>
+      <div class="card-message-name">{{ recipientName }}</div>
+      <div class="card-message-greeting">{{ $t('message.greeting') }}</div>
+      <hr class="card-message-divider" />
+      <div class="card-message-text">{{ message }}</div>
+      <div class="card-message-sender">{{ $t('message.sender', { name: senderName }) }}</div>
+    </div>
     <div class="card-message-continue">
       <button class="btn btn-primary" @click="$emit('continue')">
         {{ $t('message.continue') }}
